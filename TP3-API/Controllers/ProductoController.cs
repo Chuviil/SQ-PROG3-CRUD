53b1c6706644d5d7ec24db0ff6f271444fed8f72
Sebastian Quito <josueq12@gmail.com>
2023-10-09 16:52:42 -0500

    API sln created

---
+++ TP3-API/Controllers/ProductoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP2_CRUD.Models;
+
+namespace TP3_API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductoController : ControllerBase
+{
+    [HttpGet(Name = "GetProductos")]
+    public Producto GetAll()
+    {
+        return new Producto();
+    }
+}
