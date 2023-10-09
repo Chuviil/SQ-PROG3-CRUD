fd2ee5965e702ff65d80361367dc6538a8739bce
Sebastian Quito <josueq12@gmail.com>
2023-10-08 19:25:20 -0500

    Producto model

---
+++ TP3-API/Models/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP2_CRUD.Models;
+
+public class Producto
+{
+    public int IdProducto { get; set; }
+    [Required]
+    public string? Nombre { get; set; }
+    public string? Descripcion { get; set; }
+    [Required]
+    public int Cantidad { get; set; }
+}
